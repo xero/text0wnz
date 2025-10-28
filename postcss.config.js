c33cec2341704d0998405d3e3eaa647f8146f164
xero <x@xero.style>
2025-10-28 11:44:17 -0400

    opt(css) remove comments

--- postcss.config.js
+++ postcss.config.js
@@ -2,13 +2,18 @@ import tailwindcss from '@tailwindcss/postcss';
 import cssnano from 'cssnano';
 
 export default {
-  plugins: [
-    () => {
+	plugins: [
+		() => {
+			// build log output
 			console.log(`\n\x1b[36m[PostCSS] \x1b[0mbuilding styles:\n- \x1b[35mtailwindcss\x1b[0m\n- \x1b[35mcssnano \x1b[34m(advanced)\x1b[0m`);
-    },
-    tailwindcss,
-    cssnano({
-      preset: 'advanced',
-    }),
-  ],
+		},
+		tailwindcss,
+		cssnano({
+			preset: ['advanced', {
+				discardComments: {
+					removeAll: true,
+				},
+			}],
+		}),
+	],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports={
 	darkMode:'class',
 	content:[
-    './src/www/index.html',
-		'./src/css/editor.css',
+    './src/index.html',
+		'./src/css/style.css',
 	],
 }
